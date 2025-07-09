714e6f3f958fbdce5c6c08f4880b86990620ca6a
Greg Anderson <greg.anderson.cs@gmail.com>
2025-07-09 16:49:53 -0600

    Display border on answers marked as the right answer

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -3,7 +3,7 @@ import LikesButton from "./LikesButton";
 
 const Answer: React.FC<AnswerResponse> = ({ response, likes, answered, user }) => {
   return (
-    <div className="bg-gray-800 w-[80%] mt-2 p-5">
+    <div className={`bg-gray-800 w-[80%] mt-2 p-5 ${answered && "border-1 border-green-500 rounded"}`}>
       <p className="text-purple-400">{user}</p>
       <h1>{response}</h1>
       <div className="flex justify-end">
